15d15da4cd39fd2ab5027491ff725fab3bbf35e1
AnHiVa <anhiva97@gmail.com>
2020-08-01 00:56:03 -0600

    R code

--- pml_project.R
+++ pml_project.R
@@ -1,5 +1,9 @@
 library(caret)
 library(tidyverse)
+library(rpart)
+library(rpart.plot)
+library(rattle)
+library(corrplot)
 
 # Download datasets
 
@@ -45,14 +49,33 @@ training_small <- training %>%
   group_by(classe)
 
 #Due to the specifications in the article, we will use the gyroscope as our predictor
-training_gyros <- training_small %>%
-  select(matches('classe|gyros'))
+training_ga <- training_small %>%
+  select(matches('classe|^gyros|^accel'))
+
+set.seed(1579)
+trainingset <- createDataPartition(y = training_ga$classe, p = 0.6, list = FALSE)
+TrainTrainingSet <- training_ga[trainingset, ]
+TestTrainingSet <- training_ga[-trainingset, ]
+
+corrplot(cor(TrainTrainingSet[, -length(names(TrainTrainingSet))]), 
+         method = "color", tl.cex = 0.5)
+
+#decision tree
+set.seed(8745)
+
+model_dt <- rpart(classe~., data= TrainTrainingSet, method = 'class')
+pred_dt <- predict(model_dt, TestTrainingSet, type = 'class')
+
+fancyRpartPlot(model_dt)
+confusionMatrix(pred_dt, TestTrainingSet$classe)
 
 #Random Forest
-
 set.seed(7654)
-model_rf <- train(classe~.,data=training_gyros, method='rf')
 
-pred_rf <- predict(model_rf, testing)
+model_rf <- train(classe~.,data=TrainTrainingSet, method='rf')
+pred_rf <- predict(model_rf, TestTrainingSet)
+confusionMatrix(pred_rf, TestTrainingSet$classe)
 
-# K-Fold Cross Validation 
+#assignment answers
+answers <- predict(model_rf, testing)
+
